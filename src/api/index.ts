83b85a2596f373f8527a5a2907fd4d4e98552346
Yuri <ygonzoo@gmail.com>
2023-07-13 14:37:46 +0200

    add ID vs userID & userName
    '@ ID' -> userName
    'ID'  -> userID

--- src/api/index.ts
+++ src/api/index.ts
@@ -28,16 +28,23 @@ app.get('/api/users', async (context) => {
 });
 
 // Endpoint to get a specific user profile based on userId
-app.get('/api/users/:userId', async (context) => {
+app.get('/api/users/:ID', async (context) => {
   try {
     // Extract the userId from the request parameters
-    const userId = context.req.param('userId');
+    const ID = context.req.param('ID');
 
     // If the userName is missing, return a "Missing userId" error response with status code 400
-    if (!userId) return context.text('Missing userId', 400);
+    if (!ID) return context.text('Missing userId or @userName', 400);
+
+    if (ID.startsWith('@')) {
+      const userName = ID.slice(1);
+      // Fetch the user profile using the provided userName
+      const data = await fetchUserProfile({ userName });
+      return context.json(data);
+    }
 
     // Fetch the user profile using the provided userId
-    const data = await fetchUserProfile({ userId });
+    const data = await fetchUserProfile({ userId: ID });
 
     // Return the fetched data as a JSON response
     return context.json(data);
@@ -68,16 +75,23 @@ app.get('/api/threads/:threadId/replies', async (context) => {
 });
 
 // Endpoint to get user profile threads
-app.get('/api/users/:userId/threads', async (context) => {
+app.get('/api/users/:ID/threads', async (context) => {
   try {
     // Extract the userId from the request parameters
-    const userId = context.req.param('userId');
+    const ID = context.req.param('ID');
 
     // If the userName is missing, return a "Missing userId" error response with status code 400
-    if (!userId) return context.text('Missing userId', 400);
+    if (!ID) return context.text('Missing userId or @userName', 400);
+
+    if (ID.startsWith('@')) {
+      const userName = ID.slice(1);
+      // Fetch the user profile threads using the provided userName
+      const data = await fetchUserProfileThreads({ userName });
+      return context.json(data);
+    }
 
     // Fetch the user profile threads using the provided userId
-    const data = await fetchUserProfileThreads({ userId });
+    const data = await fetchUserProfileThreads({ userId: ID });
 
     // Return the fetched data as a JSON response
     return context.json(data);
